804dcac22dfa516d141b733d811c3a49b6501179
Mahmoud Ahmed <buttererror@gmail.com>
2019-11-21 16:06:15 +0200

    pre caching assets

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+const staticCacheName = "static-cache";
+const assets = [
+    '/',
+    '/index.html',
+    'js/app.js',
+    'js/ui.js',
+    'css/styles.css',
+    'css/materialize.min.css',
+    'img/dish.png',
+    'js/materialize.min.js',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
 // install service workersss
 self.addEventListener('install', evt => {
-    console.log("sevice worker has been installed");
+    // console.log("sevice worker has been installed");
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            cache.addAll(assets);
+        })
+    );
 });
 
 // listen to activate event
